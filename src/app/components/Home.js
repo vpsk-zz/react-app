6aed58334c3f01d861450f77a7ece18fe768dec7
virendra <vpsk05@gmail.com>
2017-03-07 23:41:03 +0530

    props uses

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,12 +2,19 @@ import React from "react";
 
 export class Home extends React.Component {
 	render() {
+		console.log(this.props);
 		return (
 			<div>
-				<p>
 					Home component
-					{<p>5*5</p>}
-				</p>
+					<p>Name : {this.props.name}</p>
+					<p>Age : {this.props.age}</p>
+					<div>
+					<p>User Name : {this.props.user.name}</p>
+						<h4>Hobbbies :</h4>
+						<ul>
+							{this.props.user.hobbies.map((hobby, key) => <li key={key}> {hobby}</li>)}
+						</ul>
+					</div>		
 			</div>
 		);
 	}
--- src/app/index.js
+++ src/app/index.js
@@ -5,8 +5,11 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
-
     render() {
+    	var user = {
+			name : "Ben",
+			hobbies : ['reading','cricket']
+		}
         return (
             <div className="container">
                 <div className="row">
@@ -16,7 +19,7 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Virendra"} age={24} user = {user}/>
                     </div>
                 </div>
             </div>
